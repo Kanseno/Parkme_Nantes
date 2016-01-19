dee99dfffe7ea5042c44bd6ae010625e01296569
Antoine <antoine@localhost.localdomain>
2016-01-19 16:47:52 +0100

    First Commit

---
+++ app/src/main/java/i5/epsi/fr/parkmenantes/MainActivity.java
@@ -0,0 +1,13 @@
+package i5.epsi.fr.parkmenantes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
